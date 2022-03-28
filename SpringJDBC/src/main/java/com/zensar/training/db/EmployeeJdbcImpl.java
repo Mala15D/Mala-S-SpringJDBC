78b94059de33bee56987819379668abf329c0801
Mala15D <92540148+Mala15D@users.noreply.github.com>
2022-03-28 12:56:41 +0530

    Update EmployeeJdbcImpl.java

--- SpringJDBC/src/main/java/com/zensar/training/db/EmployeeJdbcImpl.java
+++ SpringJDBC/src/main/java/com/zensar/training/db/EmployeeJdbcImpl.java
@@ -9,115 +9,92 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
+
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import com.zensar.training.bean.Employee;
 import com.zensar.training.bean.Gender;
 
 @Component
-public class EmployeeJdbcImpl implements EmployeeDAO {
+public class EmployeeSpringJdbcImpl implements EmployeeDAO {
 
 	@Override
-	public boolean addEmployee(Connection connection, Employee employee) throws Exception {
-		PreparedStatement statement = connection.prepareStatement(INSERT_QRY);
-		statement.setString(1, employee.getName());
-
-		LocalDate localDate = employee.getHiredDate();
-		Date dateOfJoining = DateConversion.convertToSQLDate(localDate);
-
-		statement.setDate(2, dateOfJoining);
-		statement.setString(3, employee.getGrade() + "");
-		statement.setDouble(4, employee.getBasicSalary());
-		statement.setString(5, employee.getGender().toString().charAt(0) + "");
-
-		int count = statement.executeUpdate();
-		if (count > 0)
-			return true;
-		return false;
+	public boolean addEmployee(JdbcTemplate jdbcTemplate, Employee employee) throws Exception {
+		boolean result = true;
+		Object[] rowdata = { employee.getName(), 
+				employee.getHiredDate(), 
+				employee.getGrade(),
+				employee.getBasicSalary(), 
+				employee.getGender().toString().charAt(0) };
+		int r = jdbcTemplate.update(INSERT_QRY, rowdata);
+		if (r == 0)
+			result = false;
+		return result;
 	}
 
 	@Override
-	public boolean updateEmployee(Connection connection, Employee employee) throws Exception {
-		PreparedStatement statement = connection.prepareStatement(UPDATE_QRY);
-		statement.setString(1, employee.getName());
-
-		LocalDate localDate = employee.getHiredDate();
-		Date dateOfJoining = DateConversion.convertToSQLDate(localDate);
-
-		statement.setDate(2, dateOfJoining);
-		statement.setString(3, employee.getGrade() + "");
-		statement.setDouble(4, employee.getBasicSalary());
-		statement.setString(5, employee.getGender().toString().charAt(0) + "");
-		statement.setInt(6,employee.getId());
-		int count = statement.executeUpdate();
-		if (count > 0)
-			return true;
-		return false;
-	}
-
-	@Override
-	public boolean deleteEmployee(Connection connection, Employee employee) throws Exception {
-		PreparedStatement statement = connection.prepareStatement(DELETE_QRY);
-		statement.setInt(1, employee.getId());
-		int count = statement.executeUpdate();
-		if (count > 0)
-			return true;
-		return false;
-	}
-
-	@Override
-	public Employee findEmployee(Connection connection, int empId) throws Exception {
-		PreparedStatement statement = connection.prepareStatement(FIND_QRY);
-		statement.setInt(1, empId);
-
-		ResultSet rs = statement.executeQuery();
-		if (rs.next()) {
-			Employee employee = new Employee();
-			employee.setId(rs.getInt(1));
-			employee.setName(rs.getString(2));
-
-			Date date = rs.getDate(3);
-			LocalDate doj = DateConversion.convertToLocalDate(date);
-
-			employee.setHiredDate(doj);
-			employee.setGrade(rs.getString(4).charAt(0));
-			employee.setBasicSalary(rs.getDouble(5));
-			char gender = rs.getString(6).charAt(0);
-			if (gender == 'M')
-				employee.setGender(Gender.MALE);
-			if (gender == 'F')
-				employee.setGender(Gender.FEMALE);
-			return employee;
-		}
-		return null;
-	}
-
-	@Override
-	public List<Employee> findAllEmployees(Connection connection) throws Exception {
-		List<Employee> employees=new ArrayList<>();
+	public boolean updateEmployee(JdbcTemplate jdbcTemplate, Employee employee) throws Exception {
+		boolean result = true;
+		Object[] rowdata = { employee.getName(), 
+				employee.getHiredDate(), 
+				employee.getGrade(),
+				employee.getBasicSalary(), 
+				employee.getGender().toString().charAt(0),
+				employee.getId()};
+		int r = jdbcTemplate.update(UPDATE_QRY, rowdata);
+		if (r == 0)
+			result = false;
+		return result;
 		
-		PreparedStatement statement=connection.prepareStatement(FIND_ALL_QRY);
-		ResultSet rs=statement.executeQuery();
-		while(rs.next()) {
-			Employee employee = new Employee();
-			employee.setId(rs.getInt(1));
-			employee.setName(rs.getString(2));
+	}
 
-			Date date = rs.getDate(3);
-			LocalDate doj = DateConversion.convertToLocalDate(date);
+	@Override
+	public boolean deleteEmployee(JdbcTemplate jdbcTemplate, Employee employee) throws Exception {
+	boolean result=true;
+	Object[] data= {employee.getId()};
+	
+	int r=jdbcTemplate.update(DELETE_QRY,data);
+	if(r==0)
+		result=false;
+	return result;
+	}
 
-			employee.setHiredDate(doj);
-			employee.setGrade(rs.getString(4).charAt(0));
-			employee.setBasicSalary(rs.getDouble(5));
-			char gender = rs.getString(6).charAt(0);
-			if (gender == 'M')
-				employee.setGender(Gender.MALE);
-			if (gender == 'F')
-				employee.setGender(Gender.FEMALE);
-			employees.add(employee);
-		}
+	@Override
+	public Employee findEmployee(JdbcTemplate jdbcTemplate, int empId) throws Exception {
+		Employee employee=jdbcTemplate.queryForObject(FIND_QRY, new Object[] {empId},  new EmployeeRowMapper());
+		return employee;
+	}
+
+	@Override
+	public List<Employee> findAllEmployees(JdbcTemplate jdbcTemplate) throws Exception {
+		List<Employee> employees=jdbcTemplate.query(FIND_ALL_QRY, new EmployeeRowMapper());
 		return employees;
 	}
+	private class EmployeeRowMapper implements RowMapper<Employee>{
 
+		@Override
+		public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
+			Employee employee=new Employee();
+			employee.setId(rs.getInt(1));
+			employee.setName(rs.getString(2));
+			
+			java.sql.Date dojdate=rs.getDate(3);
+			LocalDate doj= DateConversion.convertToLocalDate(dojdate);
+			
+			employee.setHiredDate(doj);
+			employee.setGrade(rs.getString(4).charAt(0));
+			employee.setBasicSalary(rs.getDouble(5));
+			
+			char gender=rs.getString(6).charAt(0);
+			if(gender=='M')
+				employee.setGender(Gender.MALE);
+			else
+				employee.setGender(Gender.FEMALE);
+			
+			return employee;
+		}
+		
+	}
 }
